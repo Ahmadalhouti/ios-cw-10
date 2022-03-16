277a4eeff381d0142b7935b86e3a6bdaf71ad8ab
Ahmad Alhouti <ahmadalh3500@gmail.com>
2022-03-16 18:51:41 +0300

    done

--- d5-cw1.playground/Contents.swift
+++ d5-cw1.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
+import Darwin
 
-var greeting = "Hello, playground"
+let family = ["Ahmad","essa","nasser","mohammad","abdullah","abdulwahed","bo ashoor","noumas"]
+
+print(family.count)
+
+var even = [2,4,6,8,10,12]
+even.remove(at:2)
+print(even)
+
+even += [16,18,20,22]
+print(even)
+
+var money = [3.00,5.00]
+money.randomElement()
+
+money.removeAll()
+
+var numbers = [1,2,3,4,5]
+
+for i in numbers{
+    print(i)
+}
+
